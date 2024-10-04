54fc66b342cca2184b80643e9b310d10339e4f67
Hugo Vega <hugo17avid@gmail.com>
2024-10-04 13:30:12 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,18 +4,23 @@ const adventurer = {
     health: 10,
     inventory: ["sword", "potion", "artifact"],
     companion: {
-      name: "Leo",
-      type: "Cat",
-      companion: {
-        name: "Frank",
-        type: "Flea",
-        inventory: ["small hat", "sunglasses"]
-      }
+        name: "Leo",
+        type: "Cat",
+        companion: {
+            name: "Frank",
+            type: "Flea",
+            inventory: ["small hat", "sunglasses"]
+        }
     },
     roll(mod = 0) {
-      const result = Math.floor(Math.random() * 20) + 1 + mod;
-      console.log(`${this.name} rolled a ${result}.`);
-      return result;
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`);
+        return result;
     }
-  };
-  
+};
+
+// Logging Robin's inventory
+console.log("Robin's inventory:");
+for (let item of adventurer.inventory) {
+    console.log(item);
+}
