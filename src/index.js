53d5ee855778bca24ad7e912b8df04ea3da26c53
Hugo Vega <hugo17avid@gmail.com>
2024-10-04 13:30:38 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -24,3 +24,21 @@ console.log("Robin's inventory:");
 for (let item of adventurer.inventory) {
     console.log(item);
 }
+
+// Part 2: Class Fantasy
+class Character {
+    static MAX_HEALTH = 100;
+  
+    constructor(name) {
+      this.name = name;
+      this.health = Character.MAX_HEALTH;
+      this.inventory = [];
+    }
+  
+    roll(mod = 0) {
+      const result = Math.floor(Math.random() * 20) + 1 + mod;
+      console.log(`${this.name} rolled a ${result}.`);
+      return result;
+    }
+  }
+  
