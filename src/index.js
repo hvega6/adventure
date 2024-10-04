702b298cc0f17627a3b4bdd29cede1943d7d2469
Hugo Vega <hugo17avid@gmail.com>
2024-10-04 13:31:05 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,3 +41,49 @@ class Character {
         return result;
     }
 }
+
+// Part 3: Class Features
+class Adventurer extends Character {
+    static ROLES = ["Fighter", "Healer", "Wizard"];
+  
+    constructor(name, role) {
+      super(name);
+      if (!Adventurer.ROLES.includes(role)) {
+        throw new Error(`Invalid role. Must be one of: ${Adventurer.ROLES.join(", ")}`);
+      }
+      this.role = role;
+      this.inventory.push("bedroll", "50 gold coins");
+    }
+  
+    scout() {
+      console.log(`${this.name} is scouting ahead...`);
+      super.roll();
+    }
+  
+    duel(opponent) {
+      console.log(`${this.name} is dueling ${opponent.name}!`);
+      while (this.health > 50 && opponent.health > 50) {
+        const thisRoll = this.roll();
+        const opponentRoll = opponent.roll();
+        if (thisRoll > opponentRoll) {
+          opponent.health -= 1;
+          console.log(`${this.name} wins this round! ${opponent.name}'s health: ${opponent.health}`);
+        } else if (opponentRoll > thisRoll) {
+          this.health -= 1;
+          console.log(`${opponent.name} wins this round! ${this.name}'s health: ${this.health}`);
+        } else {
+          console.log("It's a tie! No damage dealt.");
+        }
+      }
+      const winner = this.health > 50 ? this : opponent;
+      console.log(`The duel is over! ${winner.name} is victorious!`);
+    }
+  }
+  
+  class Companion extends Character {
+    constructor(name, type) {
+      super(name);
+      this.type = type;
+    }
+  }
+  
